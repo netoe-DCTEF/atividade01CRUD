338436b65b19513efc56aacd900d39dede3971df
weealDCTEF <elynetobr@gmail.com>
2022-07-05 22:39:34 -0300

    added hospitality

--- public/js/scriptDefaultPage.js
+++ public/js/scriptDefaultPage.js
@@ -3,5 +3,21 @@ function getDate(){
     var data_to_inner = document.querySelector('#newCorp');
     data_to_inner.innerHTML = "NEWCORP LTDA &copy" + date.getFullYear();
 }
+function getSaudacao(){
+    var saudacao = document.querySelector('#saudacoes');
+    var date = new Date();
+    var hora = date.getHours();
+    if(hora < 12){
+        saudacao.innerHTML = "Bom dia , Admin!";
+    }
+    else if(hora >=12 && hora < 18){
+        saudacao.innerHTML = "Boa Tarde , Admin!";
+    }
+    else{
+        saudacao.innerHTML = "Boa Noite , Admin!";
+    }
+
+}
 
 window.onload = getDate();
+window.onload = getSaudacao();

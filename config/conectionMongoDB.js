1ec48a2c02f7fb27a50c64aa26752074209deda0
weealDCTEF <elynetobr@gmail.com>
2022-07-16 16:22:25 -0300

    'responsivitt'

--- config/conectionMongoDB.js
+++ config/conectionMongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://ely:ely@info5.exwqa.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://ely:123@info5.exwqa.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri);
 
 module.exports = mongoose;
